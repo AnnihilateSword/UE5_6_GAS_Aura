b82fa8b4a81f69ce452621e918a72cff5d95d523
ANNIXA <743144825@qq.com>
2026-01-07 23:29:59 +0800

    <feat>: 设置 BP_AuraCharacter 角色蓝图，以及 BP_Goblin_Spear 哥布林敌人

--- Source/Aura/Character/AuraCharacterBase.cpp
+++ Source/Aura/Character/AuraCharacterBase.cpp
@@ -7,6 +7,10 @@ AAuraCharacterBase::AAuraCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+	// 初始化武器网格体组件
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::Type::NoCollision);
 }
 
 void AAuraCharacterBase::BeginPlay()
