2b3eea8aeef8f9188da6afa545154eff714193c5
AnnihilateSword <743144825@qq.com>
2026-03-07 18:06:34 +0800

    <feat>: Curve Tables - CSV and JSON

---
+++ Source/Aura/Data/CharacterClassInfo.h
@@ -0,0 +1,53 @@
+// Copyright ANNIXA.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CharacterClassInfo.generated.h"
+
+class UGameplayEffect;
+
+UENUM(BlueprintType)
+enum class ECharacterClass : uint8
+{
+	Elementalist UMETA(DisplayName = "元素师"),
+	Warrior UMETA(DisplayName = "战士"),
+	Ranger UMETA(DisplayName = "游侠")
+};
+
+USTRUCT(BlueprintType)
+struct FCharacterClassDefaultInfo
+{
+	GENERATED_BODY()
+
+	/** 这个 GE 用来初始化应用我们的主要属性 */
+	UPROPERTY(EditDefaultsOnly, Category = "Class Defaults")
+	TSubclassOf<UGameplayEffect> PrimaryAttributes;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UCharacterClassInfo : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	/** 根据角色职业获取职业默认信息 */
+	FCharacterClassDefaultInfo GetClassDefaultInfo(ECharacterClass CharacterClass);
+	
+public:
+	/** 角色职业对应的职业默认信息 */
+	UPROPERTY(EditDefaultsOnly, Category = "Character Class Defaults")
+	TMap<ECharacterClass, FCharacterClassDefaultInfo> CharacterClassInformation;
+
+	/** 这个 GE 用来初始化应用我们的次要属性 */
+	UPROPERTY(EditDefaultsOnly, Category = "Common Class Defaults")
+	TSubclassOf<UGameplayEffect> SecondaryAttributes;
+
+	/** 这个 GE 用来初始化应用我们的重要属性 */
+	UPROPERTY(EditDefaultsOnly, Category = "Common Class Defaults")
+	TSubclassOf<UGameplayEffect> VitalAttributes;
+};
