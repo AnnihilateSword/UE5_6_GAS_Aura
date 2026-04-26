16ab16f9159de0ab593e75b19255d4a928fee6fe
AnnihilateSword <743144825@qq.com>
2026-04-26 22:46:38 +0800

    <feat>: 解决 UE5 AnimNotify 在 DedicatedServer 上不一致地没有触发的问题 参考：https://forums.unrealengine.com/t/animnotify-inconsistently-doesnt-trigger-on-the-server/429809/11

---
+++ Source/Aura/Character/AuraCharacterMovementComponent.cpp
@@ -0,0 +1,16 @@
+﻿// Copyright ANNIXA.
+
+
+#include "AuraCharacterMovementComponent.h"
+
+#include "GameFramework/Character.h"
+
+void UAuraCharacterMovementComponent::ServerMove_PerformMovement(const FCharacterNetworkMoveData& MoveData)
+{
+	Super::ServerMove_PerformMovement(MoveData);
+	
+	if (CharacterOwner && !CharacterOwner->bClientUpdating && CharacterOwner->IsPlayingRootMotion() && CharacterOwner->GetMesh())
+	{
+		CharacterOwner->GetMesh()->ConditionallyDispatchQueuedAnimEvents();
+	}
+}
---
+++ Source/Aura/Character/AuraCharacterMovementComponent.h
@@ -0,0 +1,17 @@
+﻿// Copyright ANNIXA.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "AuraCharacterMovementComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class AURA_API UAuraCharacterMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+
+public:
+	virtual void ServerMove_PerformMovement(const FCharacterNetworkMoveData& MoveData) override;
+};
